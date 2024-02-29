669eeaf5266413356eaed03a94cf943f60b3e2d2
bojidaryovchev <blizzardcon10@gmail.com>
2024-02-29 19:30:17 +0200

    refactor: use lazy-loading throughout router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomePage from "../pages/HomePage.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,14 +6,11 @@ const router = createRouter({
     {
       path: "/",
       name: "home",
-      component: HomePage,
+      component: () => import("../pages/HomePage.vue"),
     },
     {
       path: "/about",
       name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import("../pages/AboutPage.vue"),
     },
   ],
