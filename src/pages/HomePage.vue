9193eb111a0298bb89f946e0818e247d4e9a83f2
bojidaryovchev <blizzardcon10@gmail.com>
2024-02-29 19:44:23 +0200

    refactor: removed boilerplate

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,9 +1,7 @@
-<script setup lang="ts">
-  import TheWelcome from "../components/TheWelcome.vue";
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <main></main>
 </template>
+
+<script lang="ts" setup></script>
+
+<style scoped></style>
